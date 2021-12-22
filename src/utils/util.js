2b611ebcb4d5b1316d9017a168a960f2bdae1b61
Tsvetan Iliev <tsvetanilievdev@gmail.com>
2021-12-22 18:17:29 +0200

    Create util.js

---
+++ src/utils/util.js
@@ -0,0 +1,21 @@
+export function saveUserData(data) {
+  sessionStorage.setItem('email', data.email);
+  sessionStorage.setItem('authToken', data.accessToken);
+  sessionStorage.setItem('id', data._id);
+
+  return {
+    'id': data._id,
+    'email': data.email,
+    'authToken': data.accessToken,
+  }
+}
+
+function removeUserData() {
+  sessionStorage.removeItem('email');
+  sessionStorage.removeItem('authToken');
+  sessionStorage.removeItem('id');
+}
+
+function isAuthenticated() {
+  return Boolean(sessionStorage.getItem('email'));
+}
