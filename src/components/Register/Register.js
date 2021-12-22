b4a09f78637d5c21c98ce7bd762022adce78fd0b
Tsvetan Iliev <tsvetanilievdev@gmail.com>
2021-12-22 21:02:31 +0200

    Update Register.js

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,9 +1,43 @@
 import styles from './Register.module.css'
+import { register } from '../../services/beautyZoneService.js'
+import { useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+import { AuthContext } from '../../contexts/AuthContext';
 
 export const Register = () => {
+const {onAuth} = useContext(AuthContext);
+const navigation = useNavigate();
+
+async function onSubmit(e) {
+  e.preventDefault();
+
+  const formData = new FormData(e.currentTarget);
+  const email = formData.get('email');
+  const password = formData.get('password');
+  const repass = formData.get('rePass');
+
+  if (email.trim() == '' || password == '') {
+    return alert('All fields must be completed!');
+  }
+
+  if(password != repass){
+    return alert('The passwords must be the same!');
+
+  }
+
+  try {
+    let result = await register(email, password);
+    onAuth(result);
+    navigation('/');
+
+  } catch (err) {
+    return alert(err.message);
+  }
+}
+
   return (
 
-    <form action="">
+    <form onSubmit={onSubmit} action="">
       <div className={styles.wrapper}>
           <h3>Register form</h3>
         <div className={styles['wrap-div']}>
@@ -12,11 +46,11 @@ export const Register = () => {
         </div>
         <div className={styles['wrap-div']}>
           <label htmlFor="password">Password: </label>
-          <input type="text" name="password" id="" />
+          <input type="password" name="password" id="" />
         </div>
         <div className={styles['wrap-div']}>
           <label htmlFor="rePass">Repeat password: </label>
-          <input type="text" name="rePass" id="" />
+          <input type="password" name="rePass" id="" />
         </div>
         <button>Submit</button>
       </div>
