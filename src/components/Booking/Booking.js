7b8fbed8b5a969460a25984338cadb3d45710f00
Tsvetan Iliev <tsvetanilievdev@gmail.com>
2021-12-23 00:07:21 +0200

    finish Booking component

--- src/components/Booking/Booking.js
+++ src/components/Booking/Booking.js
@@ -1,9 +1,26 @@
-import { createUserBooking } from '../../services/beautyZoneService';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { createUserBooking, getAll } from '../../services/beautyZoneService';
 import styles from './Booking.module.css'
 
 
 export const Booking = () => {
+    const [selectedType, setSelectType] = useState('Body');
+    const [selectedProcedures, setSelectProcedures] = useState([]);
+    const navigation = useNavigate();
     const todayString = new Date().toISOString().slice(0,10);
+    
+    useEffect(() => {
+        getAll().then(res => {
+            const filteredArr = res.filter(x => x.type == selectedType).reduce((a,c) => {a.push(c.name); return a}  , []);
+            setSelectProcedures(filteredArr)
+        });
+    }, [selectedType])
+
+    async function onChangeType(e){
+        setSelectType(e.target.value);
+    }
+
 
     async function onSubmit(e){
         e.preventDefault();
@@ -14,8 +31,7 @@ export const Booking = () => {
         const hour = formData.get('timeStart');
 
         const response = await createUserBooking({type, name, date, hour});
-        console.log(response);
-
+        navigation('/my-bookings');
     }
     return (
         <form onSubmit={onSubmit}  action="">
@@ -23,20 +39,19 @@ export const Booking = () => {
                 <h3>Create booking</h3>
                 <div className={styles['wrap-div'] + " select"}>
                     <label htmlFor="type">Select type: </label>
-                    <select name="select-type">
-                        <option value="body">Body</option>
-                        <option value="face">Face</option>
-                        <option value="hair-removal">Гair removal</option>
+                    <select onChange={onChangeType}name="select-type">
+                        <option value="Body">Body</option>
+                        <option value="Face">Face</option>
+                        <option value="Hair removal">Hair removal</option>
                     </select>
                 </div>
 
                 <div className={styles['wrap-div'] + " select"}>
                     <label htmlFor="procedure">Select Procedure: </label>
                     <select name="select-procedure">
-                        <option value="lpg">LPG</option>
-                        <option value="body">LAZER</option>
-                        <option value="remove">Remover</option>
-                        <option value="yes">yes</option>
+                        {selectedProcedures.length > 0
+                            ? selectedProcedures.map((x,i) => <option key={i} value={x}>{x}</option>)
+                            : null}
                     </select>
                 </div>
                 <div className={styles['wrap-div']}>
@@ -46,16 +61,16 @@ export const Booking = () => {
                 <div className={styles['wrap-div']}>
                     <label htmlFor="timeStart">Choose hour: </label>
                     <select name="timeStart">
-                        <option value="8">08:00</option>
-                        <option value="9">09:00</option>
-                        <option value="10">10:00</option>
-                        <option value="11">11:00</option>
-                        <option value="12">12:00</option>
-                        <option value="13">13:00</option>
-                        <option value="14">14:00</option>
-                        <option value="15">15:00</option>
-                        <option value="16">16:00</option>
-                        <option value="17">17:00</option>
+                        <option value="08:00">08:00</option>
+                        <option value="09:00">09:00</option>
+                        <option value="10:00">10:00</option>
+                        <option value="11:00">11:00</option>
+                        <option value="12:00">12:00</option>
+                        <option value="13:00">13:00</option>
+                        <option value="14:00">14:00</option>
+                        <option value="15:00">15:00</option>
+                        <option value="16:00">16:00</option>
+                        <option value="17:00">17:00</option>
                     </select>
                 </div>
                 <button>Submit</button>
